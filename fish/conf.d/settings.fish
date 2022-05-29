bf08ac784a24a2ff1e9e32e0ec3d7c35e03378ce
aitorfernandez <helloimaitor@gmail.com>
2022-05-29 09:19:32 +0100

    fish config

---
+++ fish/conf.d/settings.fish
@@ -0,0 +1,28 @@
+#!/usr/bin/env fish
+
+# List direcory contents
+alias l 'ls -la'
+alias lsa 'ls -lah'
+alias ll 'ls -l'
+
+alias c 'clear'
+alias q 'exit'
+
+# Some more alias to avoid making mistakes:
+alias rm 'rm -i'
+alias cp 'cp -i'
+alias mv 'mv -i'
+
+alias o 'open .'
+
+# Git
+alias gtheirs 'git checkout --theirs'
+alias gours 'git checkout --ours'
+
+# http://www.jukie.net/bart/blog/pimping-out-git-log
+alias glog "git log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit --date=relative"
+
+alias cb '~/.cargo/bin/cargo +nightly bench'
+alias cc '~/.cargo/bin/cargo check'
+alias cr '~/.cargo/bin/cargo run'
+alias ct '~/.cargo/bin/cargo test'
---
+++ fish/config.fish
@@ -0,0 +1,12 @@
+set -gx EDITOR vim
+set -g theme_color_scheme nord
+
+source $HOME/.config/fish/conf.d/settings.fish
+
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+# path
+# https://fishshell.com/docs/current/tutorial.html#path-example
+# fish_add_path $HOME/.cargo/bin
